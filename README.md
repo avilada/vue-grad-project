591c31df4681005708630c236cfe34464e0cecb4
underwoodbt <36803636+underwoodbt@users.noreply.github.com>
2021-10-28 19:54:36 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# vue-grad-project
-Graduate Project using Vue.js framework. Contributors: Ben Underwood and Daniel Avila
+# my-app
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+    <Header />
+    <div class="user-input">
+      <input 
+      placeholder="Press enter team name"
+      v-model="input" />
+      <button @click="searchTeam">Search Team</button>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import Header from './components/Header.vue'
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+
+.container {
+  max-width: 600px;
+  margin: 80px auto;
+}
+
+.user-input {
+  display: flex;
+  align-items: center;
+  padding-bottom: 20px;
+  input {
+    width: 100%;
+    padding: 10px 6px;
+    margin-right: 10px;
+  }
+}
+
+  button {
+  appearance: none;
+  padding: 10px;
+  font-weight: bold;
+  border-radius: 10px;
+  border: none;
+  background: grey;
+  color: white;
+  white-space: nowrap;
+
+  + button {
+    margin-left: 10px;
+  }
+}
+
+</style>
