a140a2c6f331903b533ecfa586bed094d94b2be1
underwoodbt <36803636+underwoodbt@users.noreply.github.com>
2021-10-29 10:32:50 -0400

    Added initial files
    First upload of vue files.

---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="Header">
+        <h1>Team View</h1>
+        <div class="Nav">
+            <ul>
+                <li><router-link :to="{ name: 'Home'}">Home</router-link></li>
+                <li><router-link to="/About">About</router-link></li>
+            </ul>
+        </div>
+        <router-view/>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+ul {
+    list-style-type: none;
+    text-align: center;
+    margin: 0;
+    }
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+.Nav {
+    background: #444;
+    padding: 14px 0;
+    margin-bottom: 40px;
+}
+
+.Nav a {
+    color: #fff;
+    text-decoration: none;
+    padding: 6px 8px;
+    border-radius: 10px;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+      <!-- <h1>Homepage</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur consequatur eum qui aliquam quisquam hic earum magni delectus ab, molestias eaque voluptatem, maiores tempora corrupti numquam magnam quo nisi, itaque eligendi nemo? Sapiente rem ratione rerum et adipisci illo quibusdam laboriosam quaerat fugit vitae tenetur veniam delectus laborum deleniti tempore ipsum voluptatibus eum, nostrum voluptas praesentium unde nobis doloribus eius! Fuga ea totam ipsam cupiditate, amet, tenetur iure quas, aut magni doloremque quia at fugiat dolorem optio consectetur consequatur. Expedita recusandae numquam cumque nihil officiis harum obcaecati, enim excepturi minima quibusdam sapiente ab earum cupiditate doloremque fugit esse et id.</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda similique quibusdam eaque molestiae, quaerat quis libero doloremque quas! Dolorem corrupti nemo deserunt aliquam similique temporibus ut beatae, facilis quia suscipit ipsa officiis excepturi magni dicta quo nostrum repudiandae corporis sint nam, vero ratione nisi porro. Quos earum hic unde impedit consequuntur sed, aliquam adipisci maiores enim cumque mollitia nam eius, dolorum cupiditate repellendus. Pariatur, libero hic alias cumque molestias eaque laborum quia dignissimos eos quo enim labore culpa ipsum dicta quibusdam impedit. Reiciendis, accusamus praesentium. Eveniet cum consequuntur nobis voluptatibus! Recusandae praesentium odit nam aliquam deserunt officiis et aperiam exercitationem.</p> -->
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    components: {
+    }
+}
+</script>
